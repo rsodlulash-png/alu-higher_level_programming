fa99784a45244bda1f38e8363bbd8b15f3c831c0
rsodlulash <r.sodlulash@alustudent.com>
2026-07-09 10:50:33 -0700

    Technical interview preparation

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    roman_values = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+
+    total = 0
+
+    for i in range(len(roman_string)):
+        current = roman_values[roman_string[i]]
+
+        if i + 1 < len(roman_string):
+            next_value = roman_values[roman_string[i + 1]]
+
+            if current < next_value:
+                total -= current
+            else:
+                total += current
+        else:
+            total += current
+
+    return total
