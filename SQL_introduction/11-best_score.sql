f5739ef059c8e2b46cef33773c2016e220ef6646
rsodlulash <r.sodlulash@alustudent.com>
2026-07-31 10:10:20 -0700

    a script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server.

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- lists score and name from second_table for records with score >= 10, ordered by score (highest first)
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
+
